89593aff033db76a55dc81bea672a0b0a84d9777
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-06-19 02:58:53 +0300

    Add createApp function

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { Template, templateToElement } from "./template";
+
+type App = {
+  root: HTMLElement | Text;
+};
+
+export const createApp = (selector: string, template: Template): App => {
+  // Find the root element to build app on
+  const root = document.querySelector(selector);
+  if (root == null) {
+    throw new Error("Invalid selector value!");
+  }
+
+  // create first template
+  const element = templateToElement(template);
+
+  // inject the element into the DOM
+  root.appendChild(element);
+
+  // initiate and return a new app instance
+  return {
+    root: element,
+  };
+};
