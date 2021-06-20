bfe40e0ed96164b4b6ed5a91b51b10e6a263c822
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-06-20 18:00:45 +0300

    Add component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-import { createApp } from './app';
+import { CF } from './component';
+import { templateToElement } from './template';
 
-export default {
-    createApp,
+// the Billion app type
+type Billion = {
+    render: (component: CF<undefined>) => void;
 };
+
+const Billion: Billion = {
+    render: (component) => {
+        // look for #app root element
+        const root = document.querySelector('#app');
+        if (!root) throw new Error('root element #app not found');
+        // parse and inject the component
+        const template = component();
+        const element = templateToElement(template);
+        root.appendChild(element);
+    },
+};
+
+export default Billion;
